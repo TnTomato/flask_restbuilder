a77f38480e68112c0882fcd2375331ba8832a20f
TnTomato <474093103@qq.com>
2020-09-04 13:40:44 +0800

    update requirements

--- setup.py
+++ setup.py
@@ -22,14 +22,23 @@ setup(
     python_requires='>=3.8',
     install_requires=[
         'aniso8601>=8.0.0',
+        'attrs>=20.1.0',
         'click>=7.1.2',
+        'flasgger>=0.9.5',
         'Flask>=1.1.2',
         'Flask-RESTful>=0.3.8',
+        'Flask-Script>=2.0.6',
+        'Flask-SQLAlchemy>=2.4.4',
         'itsdangerous>=1.1.0',
         'Jinja2>=2.11.2',
+        'jsonschema>=3.2.0',
         'MarkupSafe>=1.1.1',
+        'mistune>=0.8.4',
+        'pyrsistent>=0.16.0',
         'pytz>=2020.1',
+        'PyYAML>=5.3.1',
         'six>=1.15.0',
+        'SQLAlchemy>=1.3.19',
         'Werkzeug>=1.0.1'
     ],
     entry_points={'console_scripts': ['easyflask = easyflask.cli:main']}
