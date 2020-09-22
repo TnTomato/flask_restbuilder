b681b4e7019df9011467b10f048a372f9ef32cfa
TnTomato <474093103@qq.com>
2020-09-22 15:17:11 +0800

    fix docstring

--- src/flask_restbuilder/__init__.py
+++ src/flask_restbuilder/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
     flask_restbuilder
-    ~~~~~~~~~
+    ~~~~~~~~~~~~~~~~~
 
     A microframework built from Flask + Flask-Restful to make it easier to
     build Flask RESTful API projects.
